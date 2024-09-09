b704d57a931d2675153a15bdc409e9ddf7efbc09
Erik Ruotsalainen <erik.ruotsalainen1@gmail.com>
2024-09-09 13:41:58 +0300

    Added base desing + placeholder background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ProfileComponent } from './features/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProjectsComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Portfolio';
+  title = 'Portfolio - Ruotsalainen';
 }
