ca1a33469c6a6d73da75dfd654b54ef3e4000969
Supr0en <erik.ruotsalainen1@gmail.com>
2024-10-02 17:06:58 +0300

    test to push to main

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
