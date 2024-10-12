7377d9b31abdf15fcc7aa6ac8216f62c5045a4b0
Supr0en <erik.ruotsalainen1@gmail.com>
2024-10-12 22:32:07 +0300

    form komponentti lisätty sivulle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ProjectsComponent } from './features/projects/projects.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FormComponent } from './features/form/form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent, FontAwesomeModule, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, ProjectsComponent, FontAwesomeModule, RouterLink, RouterLinkActive, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,4 +17,10 @@ export class AppComponent {
   faLinkedin = faLinkedin;
   faXTwitter = faXTwitter;
   faChevronUp = faChevronUp;
+
+  @ViewChild(FormComponent) bottomSheetForm!: FormComponent;
+
+  openBottomSheet(): void {
+    this.bottomSheetForm.open();  // Trigger the opening of the bottom sheet form
+  }
 }
